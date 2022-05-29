9f6212addc7c0c4187d1ea3cc8ceecf2be42db01
Kathy Moua <kathy@page-ed.org>
2022-05-29 10:12:38 -0500

    logo change

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import coverImage from "../../assets/cover/cover-image.jpg";
+import profilephoto from "../../assets/profilephoto/Kathy-Moua.jpg";
 
 function About() {
   return (
     <section className='my-5'>
-      <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className='my-2' style={{ width: "100%"}} alt="cover"/>
+      <h1 id="about">About Me</h1>
+      <img src={profilephoto} className='my-2' style={{ width: "20%"}} alt="cover"/>
+      <p>My name is Kathy Moua.</p>
     </section>
   );
 }
